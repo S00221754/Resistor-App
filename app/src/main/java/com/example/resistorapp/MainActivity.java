2fa1a6bd886c72afa99b88b4e2d31f3a6254182a
s00221754 <s00221754@atu.ie>
2023-10-24 22:43:12 +0100

    logic and spinners working

--- app/src/main/java/com/example/resistorapp/MainActivity.java
+++ app/src/main/java/com/example/resistorapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.resistorapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.graphics.Color;
@@ -11,256 +12,158 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //EditText band1, band2, bandMulti, bandTol;
-    Spinner spin1, spin2, spin3, spin4;
-    //TextView Result;
-    Button btnCalc;
+    Spinner spinB1, spinB2, spinM, spinT;
+    TextView Result;
+    Button btnCalc, Reset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spin1 = findViewById(R.id.Band1);
-        spin2 = findViewById(R.id.band2);
-        spin3 = findViewById(R.id.band3);
-        spin4 = findViewById(R.id.band4);
-        String[] colorsBands = {"Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray", "White"};
-        String[] colorsMulti = {"Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray", "White", "Silver", "Gold"};
-        String[] colorsTol ={"Brown", "Red", "Green", "Blue", "Violet", "Gray","Silver", "Gold"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, colorsBands) {
+        spinB1 = findViewById(R.id.Band1);
+        spinB2 = findViewById(R.id.band2);
+        spinM = findViewById(R.id.band3);
+        spinT = findViewById(R.id.band4);
+        Result = findViewById(R.id.Result);
+        Reset = findViewById(R.id.btnReset);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, getResources().getTextArray(R.array.Bands)) {
             @NonNull
             @Override
-            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                TextView view = (TextView) super.getView(position, convertView, parent);
-                view.setTextColor(Color.WHITE); // Set text color to white
-                // Set background color for each item based on the associated color
-                String colorName = getItem(position);
-                int color = getColorValue(colorName); // Define a method to get the color value based on the color name
-                view.setBackgroundColor(color); // Set the background color
+            //to set the background color of the current selected item in the spinner
+            public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                view.setBackgroundColor(ItemBackgroundBand(position));
                 return view;
             }
-
+            //to set the background color of the other items
             @Override
-            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                TextView view = (TextView) super.getDropDownView(position, convertView, parent);
-                view.setTextColor(Color.WHITE); // Set text color to white
-                // Set background color for each dropdown item based on the associated color
-                String colorName = getItem(position);
-                int color = getColorValue(colorName); // Define a method to get the color value based on the color name
-                view.setBackgroundColor(color); // Set the background color for dropdown items
+            public View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                view.setBackgroundColor(ItemBackgroundBand(position));
                 return view;
             }
-
-            // Define a method to get the color value based on the color name
-            private int getColorValue(String colorName) {
-                int color;
-                switch (colorName) {
-                    case "Black":
-                        color = Color.parseColor("#FF000000");
-                        break;
-                    case "Brown":
-                        color = Color.parseColor("#FFA52A2A");
-                        break;
-                    case "Red":
-                        color = Color.parseColor("#FFFF0000");
-                        break;
-                    case "Orange":
-                        color = Color.parseColor("#FFFFA500");
-                        break;
-                    case "Yellow":
-                        color = Color.parseColor("#FFFFFF00");
-                        break;
-                    case "Green":
-                        color = Color.parseColor("#FF008000");
-                        break;
-                    case "Blue":
-                        color = Color.parseColor("#FF0000FF");
-                        break;
-                    case "Violet":
-                        color = Color.parseColor("#FF8A2BE2");
-                        break;
-                    case "Gray":
-                        color = Color.parseColor("#FF808080");
-                        break;
-                    case "Gold":
-                        color = Color.parseColor("#FFD4AF37");
-                        break;
-                    case "Silver":
-                        color = Color.parseColor("#FFC0C0C0");
-                        break;
-                    default:
-                        color = Color.BLACK; // Default color if the colorName does not match any cases
-                }
-                return color;
-            }
-
         };
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, colorsMulti) {
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, getResources().getTextArray(R.array.Multiplier)) {
             @NonNull
             @Override
-            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                TextView view = (TextView) super.getView(position, convertView, parent);
-                view.setTextColor(Color.WHITE); // Set text color to white
-                // Set background color for each item based on the associated color
-                String colorName = getItem(position);
-                int color = getColorValue(colorName); // Define a method to get the color value based on the color name
-                view.setBackgroundColor(color); // Set the background color
+            //to set the background color of the current selected item in the spinner
+            public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                view.setBackgroundColor(ItemBackgroundMulti(position));
                 return view;
             }
-
+            //to set the background color of the other items
             @Override
-            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                TextView view = (TextView) super.getDropDownView(position, convertView, parent);
-                view.setTextColor(Color.WHITE); // Set text color to white
-                // Set background color for each dropdown item based on the associated color
-                String colorName = getItem(position);
-                int color = getColorValue(colorName); // Define a method to get the color value based on the color name
-                view.setBackgroundColor(color); // Set the background color for dropdown items
+            public View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                view.setBackgroundColor(ItemBackgroundMulti(position));
                 return view;
             }
-
-            // Define a method to get the color value based on the color name
-            private int getColorValue(String colorName) {
-                int color;
-                switch (colorName) {
-                    case "Black":
-                        color = Color.parseColor("#FF000000");
-                        break;
-                    case "Brown":
-                        color = Color.parseColor("#FFA52A2A");
-                        break;
-                    case "Red":
-                        color = Color.parseColor("#FFFF0000");
-                        break;
-                    case "Orange":
-                        color = Color.parseColor("#FFFFA500");
-                        break;
-                    case "Yellow":
-                        color = Color.parseColor("#FFFFFF00");
-                        break;
-                    case "Green":
-                        color = Color.parseColor("#FF008000");
-                        break;
-                    case "Blue":
-                        color = Color.parseColor("#FF0000FF");
-                        break;
-                    case "Violet":
-                        color = Color.parseColor("#FF8A2BE2");
-                        break;
-                    case "Gray":
-                        color = Color.parseColor("#FF808080");
-                        break;
-                    case "Gold":
-                        color = Color.parseColor("#FFD4AF37");
-                        break;
-                    case "Silver":
-                        color = Color.parseColor("#FFC0C0C0");
-                        break;
-                    default:
-                        color = Color.BLACK; // Default color if the colorName does not match any cases
-                }
-                return color;
-            }
-
         };
-        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, colorsTol) {
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter adapter3 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, getResources().getTextArray(R.array.Tolerance)) {
             @NonNull
             @Override
-            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                TextView view = (TextView) super.getView(position, convertView, parent);
-                view.setTextColor(Color.WHITE); // Set text color to white
-                // Set background color for each item based on the associated color
-                String colorName = getItem(position);
-                int color = getColorValue(colorName); // Define a method to get the color value based on the color name
-                view.setBackgroundColor(color); // Set the background color
+            //to set the background color of the current selected item in the spinner
+            public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                view.setBackgroundColor(ItemBackgroundTol(position));
                 return view;
             }
-
+            //to set the background color of the other items
             @Override
-            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                TextView view = (TextView) super.getDropDownView(position, convertView, parent);
-                view.setTextColor(Color.WHITE); // Set text color to white
-                // Set background color for each dropdown item based on the associated color
-                String colorName = getItem(position);
-                int color = getColorValue(colorName); // Define a method to get the color value based on the color name
-                view.setBackgroundColor(color); // Set the background color for dropdown items
+            public View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                view.setBackgroundColor(ItemBackgroundTol(position));
                 return view;
             }
-
-            // Define a method to get the color value based on the color name
-            private int getColorValue(String colorName) {
-                int color;
-                switch (colorName) {
-                    case "Black":
-                        color = Color.parseColor("#FF000000");
-                        break;
-                    case "Brown":
-                        color = Color.parseColor("#FFA52A2A");
-                        break;
-                    case "Red":
-                        color = Color.parseColor("#FFFF0000");
-                        break;
-                    case "Orange":
-                        color = Color.parseColor("#FFFFA500");
-                        break;
-                    case "Yellow":
-                        color = Color.parseColor("#FFFFFF00");
-                        break;
-                    case "Green":
-                        color = Color.parseColor("#FF008000");
-                        break;
-                    case "Blue":
-                        color = Color.parseColor("#FF0000FF");
-                        break;
-                    case "Violet":
-                        color = Color.parseColor("#FF8A2BE2");
-                        break;
-                    case "Gray":
-                        color = Color.parseColor("#FF808080");
-                        break;
-                    case "Gold":
-                        color = Color.parseColor("#FFD4AF37");
-                        break;
-                    case "Silver":
-                        color = Color.parseColor("#FFC0C0C0");
-                        break;
-                    default:
-                        color = Color.BLACK; // Default color if the colorName does not match any cases
-                }
-                return color;
-            }
-
         };
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-
-        spin1.setAdapter(adapter);
-        spin2.setAdapter(adapter);
-        spin3.setAdapter(adapter2);
-        spin4.setAdapter(adapter3);
+        spinB1.setAdapter(adapter);
+        spinB2.setAdapter(adapter);
+        spinM.setAdapter(adapter2);
+        spinT.setAdapter(adapter3);
 
         btnCalc = findViewById(R.id.btnCalculate);
         btnCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               /* int b1,b2,m, resistance; String t, result;
-                b1 = BandColor(band1.getText().toString());
-                b2 = BandColor(band2.getText().toString());
-                m = BandMulti(bandMulti.getText().toString());
-                t = BandTolerance(bandTol.getText().toString());
+                int b1,b2,m, resistance; String t;
 
-                resistance = ((b1 * m) + b2) * m;
+                b1 = BandColor(spinB1.getSelectedItem().toString());
+                b2 = BandColor(spinB2.getSelectedItem().toString());
+                m = BandMulti(spinM.getSelectedItem().toString());
+                t = BandTolerance(spinT.getSelectedItem().toString());
+                if (m == 0){
+                    resistance = b1 + b2;
+                }
+                else {
+                    resistance = ((b1 * m) + b2) * m;
+                }
 
                 Result.setText(String.valueOf(resistance) + "Ω " + t);
-                */
+
             }
         });
     }
+    private int ItemBackgroundBand(int position) {
+        int[] colors = {R.color.black, R.color.black, R.color.brown, R.color.red, R.color.orange, R.color.yellow,
+                R.color.green, R.color.blue, R.color.violet, R.color.gray, R.color.white};
+
+        if (position >= 1 && position < colors.length) {
+            return ContextCompat.getColor(this, colors[position]);
+        }
+        else
+        {
+            return Color.TRANSPARENT;
+        }
+    }
+    private int ItemBackgroundMulti(int position) {
+        int[] colors = {R.color.black, R.color.black, R.color.brown, R.color.red, R.color.orange, R.color.yellow,
+                R.color.green, R.color.blue, R.color.violet, R.color.gray, R.color.white, R.color.gold, R.color.silver};
+
+        if (position >= 1 && position < colors.length) {
+            return ContextCompat.getColor(this, colors[position]);
+        }
+        else
+        {
+            return Color.TRANSPARENT;
+        }
+    }
+    private int ItemBackgroundTol(int position) {
+        int[] colors = {R.color.brown, R.color.brown, R.color.red,
+                R.color.green, R.color.blue, R.color.violet, R.color.gray, R.color.gold, R.color.silver};
+
+        if (position >= 1 && position < colors.length) {
+            return ContextCompat.getColor(this, colors[position]);
+        }
+        else
+        {
+            return Color.TRANSPARENT;
+        }
+    }
+    public void reset(View v){
+
+        spinB1.setSelection(0);
+        spinB2.setSelection(0);
+        spinT.setSelection(0);
+        spinM.setSelection(0);
+        Result.setText("");
+    }
+
     public Integer BandColor(String color){
         Integer band;
         switch (color){
@@ -378,4 +281,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return tolerance;
     }
+
 }
